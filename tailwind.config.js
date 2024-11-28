fe43b59f1495c1729cb61bddf78557f979e77f6c
balyuntaja <balyuntaja28@gmail.com>
2024-11-28 16:35:08 +0700

    create navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       colors: {
-        primary: '#14b8a6',
+        primary: '#78B3CE',
+        secondary: '#64748b',
         dark: '#0f172a',
       },
+      screens: {
+        '2xl': '1320px',
+      }
     },
   },
   plugins: [],
